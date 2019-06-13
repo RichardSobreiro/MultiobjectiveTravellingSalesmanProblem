7434983eb6c9c81580cfa99350e5b25b6aeb2eb8
richard.sobreiro <richard.sobreiro@gmail.com>
2019-06-12 23:15:34 -0300

    FIT: weighted solution

---
+++ CrossCutting/CrossCutting.cs
@@ -0,0 +1,65 @@
+﻿using MathNet.Numerics.LinearAlgebra.Complex;
+using System;
+using System.Linq;
+
+namespace CrossCutting
+{
+    public static class CrossCutting
+    {
+        public static double[][] MinMaxScaleDoubleArrayArray(double[][] array, int m, int n)
+        {
+            double[][] result = AllocateArrayArray(m,n);
+
+            double max = array.Cast<double>().Max();
+            double min = array.Cast<double>().Min();
+            double sum = array.Cast<double>().Sum();
+            double mean = sum / (m * n);
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[i][j] = (array[i][j] - mean) / (max - min);
+                }
+            }
+
+            return result;
+        }
+
+        public static double[][] UnitVectorScaleDoubleArrayArray(double[][] array, int m, int n)
+        {
+            double[][] result = AllocateArrayArray(m, n);
+            double[] euclideanNorms = new double[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                double linePowered2 = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    linePowered2 += array[i][j] * array[i][j];
+                }
+                euclideanNorms[i] = Math.Sqrt(linePowered2);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[i][j] = array[i][j] / euclideanNorms[i];
+                }
+            }
+
+            return result;
+        }
+
+        public static double[][] AllocateArrayArray(int m, int n)
+        {
+            double[][] result = new double[m][];
+            for(int i = 0; i < m; i++)
+            {
+                result[i] = new double[n];
+            }
+            return result;
+        }
+    }
+}
---
+++ MillerTuckerZemlinFormulation/Program.cs
@@ -0,0 +1,184 @@
+﻿using ILOG.Concert;
+using ILOG.CPLEX;
+using System.Diagnostics;
+using System.IO;
+
+namespace MillerTuckerZemlinFormulation
+{
+    public class Program
+    {
+        internal class Data
+        {
+            internal int n;
+            internal double[][] timeNormalized;
+            internal double[][] distanceNormalized;
+            internal double[][] time;
+            internal double[][] distance;
+
+            internal Data(string filename)
+            {
+                InputDataReader reader = new InputDataReader(filename);
+
+                time = reader.ReadDoubleArrayArray();
+                distance = reader.ReadDoubleArrayArray();
+                n = time.Length; 
+
+                timeNormalized = CrossCutting.CrossCutting.UnitVectorScaleDoubleArrayArray(time, n, n);
+                distanceNormalized = CrossCutting.CrossCutting.UnitVectorScaleDoubleArrayArray(distance, n, n);
+            }
+        }
+
+        static void Main()
+        {
+            try
+            {
+                string resultFilename = "./ResultFiles/MTSP_MTZ.csv";
+                string filename = "./DataFiles/Data.dat";
+                Data data = new Data(filename);
+
+                double timeFactor = 0;
+                double distanceFactor = 1;
+                double step = 0.1;
+                int routeCounter = 0;
+
+                File.WriteAllText(resultFilename, "");
+
+                do
+                {
+                    using (Cplex cplex = new Cplex())
+                    {
+                        #region [Decision Variables]
+                        IIntVar[][] x = new IIntVar[data.n][];
+                        IIntVar[] u = cplex.IntVarArray(data.n, 0, (data.n - 1));
+                        cplex.Add(u);
+
+                        for (int i = 0; i < data.n; i++)
+                        {
+                            x[i] = cplex.BoolVarArray(data.n);
+                            cplex.Add(x[i]);
+                        }
+                        #endregion
+                        #region [Objective Function]
+                        INumExpr obj = cplex.NumExpr();
+                        for (int i = 0; i < data.n; i++)
+                        {
+                            for (int j = 0; j < data.n; j++)
+                            {
+                                if (i != j)
+                                {
+                                    obj = cplex.Sum(obj,
+                                        cplex.Prod(
+                                            (
+                                                (timeFactor * data.timeNormalized[i][j]) +
+                                                (distanceFactor * data.distanceNormalized[i][j])
+                                            ), x[i][j]));
+                                }
+                            }
+                        }
+                        cplex.AddMinimize(obj);
+                        #endregion
+
+                        #region [Restrictions]
+                        for (int j = 0; j < data.n; j++)
+                        {
+                            ILinearNumExpr sumj = cplex.LinearNumExpr();
+                            for (int i = 0; i < data.n; i++)
+                            {
+                                if (i != j)
+                                {
+                                    sumj.AddTerm(1, x[i][j]);
+                                }
+                            }
+                            cplex.AddEq(sumj, 1);
+                        }
+
+                        for (int i = 0; i < data.n; i++)
+                        {
+                            ILinearNumExpr sumi = cplex.LinearNumExpr();
+                            for (int j = 0; j < data.n; j++)
+                            {
+                                if (i != j)
+                                {
+                                    sumi.AddTerm(1, x[i][j]);
+                                }
+                            }
+                            cplex.AddEq(sumi, 1);
+                        }
+
+                        for (int i = 0; i < data.n; i++)
+                        {
+                            for (int j = 0; j < data.n; j++)
+                            {
+                                if (i >= 1 && i != j && j < data.n)
+                                {
+                                    cplex.AddLe(
+                                            cplex.Sum(cplex.Diff(u[i], u[j]),
+                                                cplex.Prod(data.n , x[i][j])),
+                                        (data.n - 1));
+                                }
+                            }
+                        }
+                        #endregion
+
+                        Stopwatch stopWatch = new Stopwatch();
+                        stopWatch.Start();
+                        if (cplex.Solve())
+                        {
+                            stopWatch.Stop();
+                            double[][] sol_x = new double[data.n][];
+                            for (int i = 0; i < data.n; i++)
+                                sol_x[i] = cplex.GetValues(x[i]);
+                            double[] sol_u = new double[data.n];
+                            sol_u = cplex.GetValues(u);
+
+                            double timeTotal = 0;
+                            double distanceTotal = 0;
+                            for (int i = 0; i < data.n; i++)
+                            {
+                                for (int j = 0; j < data.n; j++)
+                                {
+                                    timeTotal += data.time[i][j] * sol_x[i][j];
+                                    distanceTotal += data.distance[i][j] * sol_x[i][j];
+                                }
+                            }
+
+                            StreamWriter file = new StreamWriter(resultFilename, true);
+                            file.WriteLine($"{timeFactor},{distanceFactor},{stopWatch.Elapsed.TotalSeconds},{cplex.ObjValue},{timeTotal},{distanceTotal}");
+                            file.Close();
+
+                            StreamWriter fileRouteResult = new StreamWriter($"./ResultFiles/Route-{routeCounter}.txt");
+                            for (int i = 0; i < data.n; i++)
+                            {
+                                for (int j = 0; j < data.n; j++)
+                                {
+                                    if(sol_x[i][j] == 1)
+                                    {
+                                        fileRouteResult.WriteLine($"From city {i} to city {j} - u[i] = {sol_u[i]} and u[j] = {sol_u[j]}");
+                                    }
+                                }
+                            }
+                            fileRouteResult.Close();
+                        }
+                        cplex.End();
+                    }
+
+                    timeFactor += step;
+                    distanceFactor -= step;
+                    routeCounter++;
+                } while (timeFactor <= 1);
+            }
+            catch (ILOG.Concert.Exception ex)
+            {
+                System.Console.WriteLine("Concert Error: " + ex);
+            }
+            catch (InputDataReader.InputDataReaderException ex)
+            {
+                System.Console.WriteLine("Data Error: " + ex);
+            }
+            catch (System.IO.IOException ex)
+            {
+                System.Console.WriteLine("IO Error: " + ex);
+            }
+        }
+    }
+}
